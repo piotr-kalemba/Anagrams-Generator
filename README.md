486a58065d0607f57b208664819e8c41d79aa21a
piotr-kalemba <piotr-kalemba@wp.pl>
2020-03-01 20:47:44 +0100

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Anagrams-Generator
+The project provides a relatively simple tool for generating all the anagrams of a given phrase based
+on given vocabulary list. The order of the words in each anagram is inherited from the order
+in the vocabulary list, which ensures that every anagram is generated only once (permutations
+of the same anagram are skipped). 
+
+
+The project consists of the following files:
+1) corncob_lowercase.txt - an English vocabulary list
+2)  load_vocab.py - module that includes a function for turning the vocabulary list
+into a python list
+3) generate_anagrams.py - core module of the project with the function anagrams. It is a recursive function that adds 
+successively generated results to a file. After execution of the main function within the
+module the file with the results is saved in the main directory. 
--- generate_anagrams.py
+++ generate_anagrams.py
@@ -20,26 +20,25 @@ def fit_in(word, name):
 def anagrams(name, words, p, phrase, file, ind, maximum):
     """Recursive function that generates all anagrams of the 'phrase' parameter comprised of words from the list
     'words' and write them to the 'file' parameter. Each anagram of the 'phrase' is given in the order of the
-    'words'-list, which excludes generation of permutations of the same anagram."""
+    'words'-list, thereby excluding  generation of multiply permutations of the same anagram."""
     used = list(''.join(phrase[:p]))
-    index = 0 if p == 0 else words.index(phrase[p-1])
+    index = 0 if p == 0 else words.index(phrase[p - 1])
     if len(used) == len(name):
         file.write(' '.join(phrase[:p]) + '\n')
-        index = ind[0] + 1
-        ind[0] = index
-        "when the number maximum of lines written down in the file is reached, the program terminates"
-        if index == maximum:
+        ind[0] += 1
+        "when the maximum number of lines written to the file is reached, the program terminates"
+        if ind[0] == maximum:
             sys.exit()
     else:
         left_over = list(name)
         for letter in used:
             left_over.remove(letter)
         for word in words[index:]:
-            "the slicing of the list 'words' ensures that the phrases in the generated anagram" \
-                             "occur in the order of words-list"
+            "above slicing of the list 'words' ensures that the words in the generated anagram" \
+                 "occur in the order of the words-list"
             if fit_in(word, left_over):
                 phrase[p] = word
-                anagrams(name, words, p+1, phrase, file, ind, maximum)
+                anagrams(name, words, p + 1, phrase, file, ind, maximum)
 
 
 def gen_anagrams(name, words, file, maximum):
@@ -49,6 +48,8 @@ def gen_anagrams(name, words, file, maximum):
 
 
 def main():
+    """the main function dynamically creates a file containing list of anagrams of a given phrase of the length being
+    at most the value of the 'maximum' variable"""
     words = set(load_words())
     name = input('Enter your name: ')
     maximum = int(input('Set limit on the number of anagrams to write down in the file: '))
